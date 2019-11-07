33c73a206eccc7862cc0b03fbf177504852e12de
troy82086 <turner82086@gmail.com>
2019-11-07 15:26:05 -0700

    Fix broken code

--- WelcomePage.xaml.cs
+++ WelcomePage.xaml.cs
@@ -21,9 +21,9 @@ namespace BTPaint
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class WelcomePage : Page
     {
-        public MainPage()
+        public WelcomePage()
         {
             this.InitializeComponent();
         }
