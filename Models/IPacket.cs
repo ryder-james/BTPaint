f61e0e668039d6fe413d4c17885b3340427cac0f
Ragger15 <carsonsena@gmail.com>
2019-11-20 16:27:15 -0700

    Remove unneeded handler

--- Models/IPacket.cs
+++ Models/IPacket.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace Networking.Models
 {
-    public delegate byte[] PacketReadyEventHandler();
-
     public interface IPacket
     {
         byte[] ToByteArray();
