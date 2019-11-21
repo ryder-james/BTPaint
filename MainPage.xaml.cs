7faa21afe4067cbcf6c3cac4578076136c1088d1
Eric Poppleton <Vehin77@hotmail.com>
2019-11-21 16:31:59 -0700

    Add data binding to PolySides

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -224,6 +224,7 @@ namespace BTPaint
             starBtn.Background = new SolidColorBrush(Colors.Gray);
             sidesText.Visibility = Visibility.Collapsed;
             sidesSlider.Visibility = Visibility.Collapsed;
+            sidesSlider.Value = 1;
         }
 
         private void eraserBtn_Click(object sender, RoutedEventArgs e)
@@ -234,6 +235,7 @@ namespace BTPaint
             starBtn.Background = new SolidColorBrush(Colors.Gray);
             sidesText.Visibility = Visibility.Collapsed;
             sidesSlider.Visibility = Visibility.Collapsed;
+            sidesSlider.Value = 1;
         }
 
         private void starBtn_Click(object sender, RoutedEventArgs e)
@@ -244,6 +246,7 @@ namespace BTPaint
             starBtn.Background = new SolidColorBrush(Colors.White);
             sidesText.Visibility = Visibility.Visible;
             sidesSlider.Visibility = Visibility.Visible;
+            sidesSlider.Value = 3;
         }
     }
 }
